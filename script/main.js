5ea09be07d904664ef0da4b4412564bd820944b9
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2021-02-27 12:45:56 +0300

    newTest

--- script/main.js
+++ script/main.js
@@ -1,14 +1,19 @@
-let editBtn = document.querySelector('.edit-button');
+let editBtn = document.querySelector('.profile__edit-button');
 let popUp = document.querySelector('.pop-up');
 let popUpActive = document.querySelector('.pop-up_active');
-let popUpClose = document.querySelector('.pop-up__close');
+let popUpClose = document.querySelector('.pop-up__btn-close');
 
+// находим имя и работу user
+let userName = document.querySelector('.profile__info');
+let userJob = document.querySelector('.profile__text');
 
-const openPopUp = () => {
+let openPopUp = () => {
+  nameInput.value = userName.textContent;
+  jobInput.value = userJob.textContent;
   popUp.classList.add('pop-up_active');
 }
 
-const closePopUp = () => {
+let closePopUp = () => {
   popUp.classList.remove('pop-up_active');
 }
 
@@ -17,3 +22,29 @@ popUpClose.addEventListener('click', closePopUp);
 editBtn.addEventListener('click', openPopUp);
 
 
+// Находим форму в DOM
+let formElement = document.querySelector('.pop-up__form');
+// Находим поля формы в DOM
+let nameInput = formElement.querySelector('#nameInput');
+let jobInput = formElement.querySelector('#jobInput');
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+
+    // Получите значение полей jobInput и nameInput из свойства value
+
+
+    // Выберите элементы, куда должны быть вставлены значения полей
+
+
+    // Вставьте новые значения с помощью textContent
+    userName.textContent = nameInput.value;
+    userJob.textContent = jobInput.value;
+    closePopUp();
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler);
